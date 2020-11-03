422e54b86d4ed524aa763f95b0f845cdf58b709c
wangyulue <1206180097@qq.com>
2020-11-04 01:09:11 +0800

    article(*)从零构建一个简单的 react

---
+++ article01/demo02/index.tsx
@@ -0,0 +1,36 @@
+function createElement(type: string, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === 'object'
+          ? child
+          : createTextElement(child)
+      )
+    }
+  };
+}
+
+function createTextElement(text: string) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  };
+}
+
+/** @jsx createElement */
+const element = (<div title='foo'>
+  <span id='a'>hello</span>
+  <span id='b'>world</span>
+</div>);
+
+document.getElementById('root').innerText = JSON.stringify(element, null, 2);
+
+console.log(element);
+
+
+export { };
